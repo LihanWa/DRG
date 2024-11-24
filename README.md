69bc8ee7cf6b4f8419b36fc54fbd7ff983d896c0
Lihan Wang <liw130@pitt.edu>
2024-11-24 20:27:18 +0000

    open source

---
+++ README.md
@@ -0,0 +1,56 @@
+# DRG and Patient Severity
+#### 1.Our code is for MS-DRG and APR-DRG of MIMIC-III, and APR-DRG of MIMIC-IV, by setting "--data_source". 
+
+#### 2.Our code is for our model (LGLC) and Clinical LongFormer by setting "--LongFormer". 
+
+#### 3. For Clinical LongFormer, you can choose to use our abbreviation_fullspelling.txt file to replace abbreviation in the clinical texts with fullspelling, by setting "--use_AbbFull".
+
+#### 4. Our code is for both training and testing by setting "--eval_model".
+## 1.combine information from files and preprocess:
+Get files for MS-DRG and APR-DRG of MIMIC-III: 
+
+DRGCODES.csv, ICUSTAYS.csv, ADMISSIONS.csv, PATIENTS.csv, and NOTEEVENTS.csv are available in [MIMIC_III](https://physionet.org/content/mimiciii/1.4/).
+
+Get files for APR-DRG of MIMIC-IV: 
+
+DRGCODES.csv, ICUSTAYS.csv, ADMISSIONS.csv, and PATIENTS.csv are available in [MIMIC_IV](https://physionet.org/content/mimiciv/2.2/). discharge.csv is available in [discharge](https://physionet.org/content/mimic-iv-note/2.2/note/#files-panel).
+
+The method of combining information from files is the same whether it is for LongFormer or not. "--collectText" can be set as "No", if "text_raw" file is already created. For data preprocess, if you want to preprocess LongFormer, the "--LongFormer" should be set as "Yes"; otherwise, it should be set as "No". 
+```shell
+#--mimic_dir depends on where your mimic files are.
+!python data_preprocess.py --data_dir data --mimic_dir  --threshold 48 --data_source ms --LongFormer Yes --collectText No --use_AbbFull No
+```
+BioWordVec_PubMed_MIMICIII_d200.vec.bin
+The [BioWordVec](https://github.com/ncbi-nlp/BioSentVec) word embedding is used in LGLC model, and it is better to be put in the root directory. 
+## 2.create dataset
+```shell
+!python create_dataset.py  --data_source ms --LongFormer Yes --use_AbbFull No
+```
+
+## Eval:
+
+To run our results directly: Firstly, you should finish running step 1-2. Then, you can download the checkpoints from the links and move it to the root directory. 
+
+The "--eval_model" should be set as eval.
+
+If you want to run LongFormer, the "--LongFormer" should be set as "Yes"; otherwise, it should be set as "No". If running LongFormer, the "--batch_size" depends on your GPU RAM and System RAM. I set it as 3.
+
+The "--data_source" can be set as "ms", or "apr_mimic3", or "apr_mimic4", which represents MS-DRG and APR-DRG of MIMIC-III, and APR-DRG of MIMIC-IV respectively.
+
+```shell
+#If you want to train by yourself, "--eval_model" should be set as "train".
+!python main.py --epochs 1  --lr 1.2e-4 --wd 0 --data_source apr_mimic3 --eval_model eval --LongFormer Yes --batch_size 3
+```
+### checkpoints:
+checkpoint for MS-DRG of MIMIC-III of LGLC: [bestMS](https://drive.google.com/file/d/1I-XlJP0Gj3GK6U4ebRpxn0PwtHdZIoK4/view?usp=sharing)
+
+checkpoint for APR-DRG of MIMIC-III of LGLC: [bestAPR_MIMIC3](https://drive.google.com/file/d/1-QrKJ2wR5fHsMxMZVhoE_AZD46kRR8zx/view?usp=sharing)
+
+checkpoint for APR-DRG of MIMIC-IV of LGLC: [bestAPR_MIMIC4](https://drive.google.com/file/d/1-RQsxiwJGNa2sPZmJASfs1trHsG6Tw3E/view?usp=sharing)
+
+checkpoint for MS-DRG of MIMIC-III of LongFormer: [bestMS_LongFormer](https://drive.google.com/file/d/1-If6pLWlqAPEEkc0lE_eC07dCDaRkjc1/view?usp=sharing)
+
+checkpoint for APR-DRG of MIMIC-III of LongFormer: [bestAPR_MIMIC3_LongFormer](https://drive.google.com/file/d/1--IU-v4MxLvD7aaRw2oHjRFKTolInfK_/view?usp=sharing)
+
+checkpoint for APR-DRG of MIMIC-IV of LongFormer: [bestAPR_MIMIC4_LongFormer](https://drive.google.com/file/d/1Jc1auu8L9nGKyAlP3p7YoGs9HtYJDVvB/view?usp=sharing)
+
---
+++ main.py
@@ -0,0 +1,79 @@
+import pandas as pd
+import numpy as np
+import pickle as pk
+import os
+import time
+import copy
+import torch
+import torch.nn as nn
+from train_utils import train_dev, eval_test
+from eval_utils import full_metrics, result2str, reg_metrics, score_f1, score_mae
+from data_utils_ori_2000 import DrgTextDataset, load_rule
+from models import Attention
+from options import args
+from models import model
+from transformers import AutoTokenizer, LongformerForSequenceClassification,Trainer,AutoModel,LongformerTokenizerFast,RobertaTokenizerFast, RobertaForSequenceClassification, TrainingArguments
+os.environ['CUDA_VISIBLE_DEVICES'] = args.device
+
+
+
+if __name__ == '__main__':
+    if args.data_source=='ms':
+        RULE_PATH = 'rules/MSDRG_RULE13.csv' 
+    else:
+        RULE_PATH = 'rules/APRDRG_RULE13.csv' 
+    data_dir = '%s/%s' % ('data',  args.data_source)
+    TEXT_DIR = '%s/text_tokens' % data_dir
+    if args.LongFormer=='No':
+      embedding = np.load('%s/embeddingAug.npy' % TEXT_DIR) 
+      print(embedding.shape)
+    run_time = time.strftime('%b_%d_%H_%M', time.localtime())
+    if args.LongFormer=='No':
+        with open('test_dataset_%s.pkl'% args.data_source, "rb") as file: 
+            test_dataset = pk.load(file)
+    else:
+        with open('test_dataset_%s_LongFormer.pkl'% args.data_source, "rb") as file: 
+            test_dataset = pk.load(file)
+    score_func = score_f1
+
+    if args.LongFormer=='Yes':
+        if args.data_source=='ms':
+            model = LongformerForSequenceClassification.from_pretrained("yikuan8/Clinical-Longformer",num_labels=738,ignore_mismatched_sizes=True)
+        else:
+            model = LongformerForSequenceClassification.from_pretrained("yikuan8/Clinical-Longformer",num_labels=1136,ignore_mismatched_sizes=True)
+        optimizer = torch.optim.AdamW(model.parameters(), lr=2e-5, weight_decay=1e-4)
+    else:
+        Atten=Attention(input_size=512,output_size=150) 
+        model = model(args, embedding,Atten,0)    
+        optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.wd)
+        model=model.cuda()
+    
+    if args.eval_model == 'train':
+        print('train')
+        if args.LongFormer=='Yes':
+            state_dict = torch.load("best%s_LongFormer" % args.data_source.upper())['model_state_dict']   
+            model.load_state_dict(state_dict)
+
+            # num_layers_to_freeze=5
+            # for param in model.longformer.encoder.layer[:num_layers_to_freeze].parameters():
+            #     param.requires_grad = False
+            with open('dev_dataset_%s_LongFormer.pkl'% args.data_source, "rb") as file: 
+                dev_dataset = pk.load(file)
+            with open('train_dataset_%s_LongFormer.pkl'% args.data_source, "rb") as file: 
+                train_dataset = pk.load(file)                
+            
+        else:
+            with open('train_dataset_%s.pkl'% args.data_source, "rb") as file: 
+                train_dataset = pk.load(file)
+            with open('dev_dataset_%s.pkl'% args.data_source, "rb") as file: 
+                dev_dataset = pk.load(file)
+        model_wts = train_dev(model,train_dataset, dev_dataset, args.epochs,  args.batch_size, optimizer, score_func)
+    else:
+        print('eval')
+        if args.LongFormer=='No':
+            state_dict = torch.load("best%s" % args.data_source.upper())['model_state_dict']   
+        else:
+            state_dict = torch.load("best%s_LongFormer" % args.data_source.upper())['model_state_dict']   
+        model.load_state_dict(state_dict)
+        
+    te_score, te_inf = eval_test(model,test_dataset, score_func, RULE_PATH, True, args.batch_size)
